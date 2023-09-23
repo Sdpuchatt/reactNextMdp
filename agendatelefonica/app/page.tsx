b866c1a43e82ac13d7f3fa1487143042617f1385
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2023-09-23 10:54:57 -0300

    main next
    instalamos e iniciamos next js

---
+++ agendatelefonica/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>Hola mundo</p>
+        <p></p>
+        <p></p>
+    </main>
+  )
+}
+// npm install -g create-react-app
+// npx create-next-app my aplication
